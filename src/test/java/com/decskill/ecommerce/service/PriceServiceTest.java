6b029291147cd783fa9843d897830d85d153cb54
Alexei7ca <97787086+Alexei7ca@users.noreply.github.com>
2024-09-03 23:34:05 +0200

    ADD: readme and extra tests

--- src/test/java/com/decskill/ecommerce/service/PriceServiceTest.java
+++ src/test/java/com/decskill/ecommerce/service/PriceServiceTest.java
@@ -13,10 +13,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class PriceServiceTest {
@@ -69,6 +72,33 @@ public class PriceServiceTest {
         Price found = pricesService.getPrice(testDate, testBrand, testProductId);
         assertThat(found).isEqualTo(testPriceInfo);
         verify(pricesRepository).findFirstPriceByProductBrandAndDate(testDate, testBrand, testProductId);
+    }
+
+    @Test
+    public void whenMultipleThreadsGetPrice_thenReturnConsistentResults() throws InterruptedException {
+        LocalDateTime testDate = LocalDateTime.now();
+        Brand testBrand = new Brand();
+        testBrand.setName("testBrand");
+        testBrand.setId(5);
+        int testProductId = 1;
+
+        when(pricesRepository.findFirstPriceByProductBrandAndDate(testDate, testBrand, testProductId)).thenReturn(testPriceInfo);
+
+        int numberOfThreads = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.submit(() -> {
+                Price found = pricesService.getPrice(testDate, testBrand, testProductId);
+                assertThat(found).isEqualTo(testPriceInfo);
+            });
+        }
+
+        executorService.shutdown();
+        boolean finished = executorService.awaitTermination(1, TimeUnit.MINUTES);
+        assertTrue(finished, "ExecutorService did not terminate in time");
+
+        verify(pricesRepository, times(numberOfThreads)).findFirstPriceByProductBrandAndDate(testDate, testBrand, testProductId);
     }
 
 }
